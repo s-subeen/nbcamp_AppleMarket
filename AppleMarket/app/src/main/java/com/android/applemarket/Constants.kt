e20f8c0c8bd10abb5070fe1a84af022b3a89f9f9
s-subeen <101384641+s-subeen@users.noreply.github.com>
2024-01-11 20:29:15 +0900

    Update: 텍스트 밑줄

--- AppleMarket/app/src/main/java/com/android/applemarket/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.android.applemarket
-
-object Constants {
-    const val EXTRA_PRODUCT_ENTITY = "extra_product_entity"
-    const val NOTIFICATION_CHANNEL_ID = "one-channel"
-    const val NOTIFICATION_ID = 11
-    const val EXTRA_PRODUCT_ID = "extra_product_id"
-    const val EXTRA_PREFERENCE_STATUS = "extra_preference_status"
-}
